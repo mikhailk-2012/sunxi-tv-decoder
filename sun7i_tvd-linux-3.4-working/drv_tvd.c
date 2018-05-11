mikhailk-2012/sunxi-tv-decoder

commit 16722b15ecdaa15321f662dfeec93ace50abccdf

Avoid cummulated frames that cannot be processed on time
Same patch than github user milosladni. This is his explanation:

When you use read buffers direct for display preview there is not
problem because there are not time delay but when you would like
to do something else for example compress frame to jpeg you will
have some time delay if you do not do it speed enough and then
you will miss out some frames.
In that case driver will stuck. If you allocate for example 10
buffers first 9 buffers will has picture from fist filing up and
there stuck. Only last buffer will keep going to fill up with
correct frame value.

sun7i_tvd-linux-3.4-working/drv_tvd.c
@@ -137,6 +137,7 @@ static irqreturn_t tvd_irq(int irq, void *priv)
 	/* Nobody is waiting on this buffer*/
 	if (!waitqueue_active(&buf->vb.done)) {
 		__dbg("Nobody is waiting on this buffer,buf = 0x%p (buffer %d)\n", buf, buf->vb.i);
+		goto unlock; // skip this frame data, don't dequeue buffer and overwrite it with next frame data
 	}
 	
 	list_del(&buf->vb.queue);

commit e81ad46b700b5c61d7fdcd74c3f0f7b845584b1f

Fix bug introduced in commit 16722b15e
Commit 16722b15e intended to fix a bug that made that all buffers
except the last one retained the first frame they were filled in the
case that buffers can't be processed quickly enough.

This fix worked but it broke non-blocking buffers dequeuing, used
for example by ffmpeg. Because of this, the driver failed to dequeue
any buffer for programs such as ffmpeg.

This commit undo commit 16722b15e and fix the bug in another way that
works for both blocking and non-blocking modes.

sun7i_tvd-linux-3.4-working/drv_tvd.c
@@ -285,7 +285,6 @@ static irqreturn_t tvd_irq(int irq, void *priv)
 	/* Nobody is waiting on this buffer*/
 	if (!waitqueue_active(&buf->vb.done)) {
 		__dbg("Nobody is waiting on this buffer,buf = 0x%p (buffer %d)\n", buf, buf->vb.i);
-		goto unlock; // skip this frame data, don't dequeue buffer and overwrite it with next frame data
 	}
 	
 	list_del(&buf->vb.queue);
@@ -1262,8 +1261,8 @@ static void buffer_queue(struct videobuf_queue *vq, struct videobuf_buffer *vb)
 
 	__dbg("%s: buffer %d\n", __FUNCTION__, vb->i);
 	buf->vb.state = VIDEOBUF_QUEUED;
-/////	if (list_empty(&vidq->active))
-/////		set_addr(dev, buf);
+	if (list_empty(&vidq->active))
+		set_addr(dev, buf);
 	list_add_tail(&buf->vb.queue, &vidq->active);
 }
 